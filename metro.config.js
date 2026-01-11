24d6680ddedccd70ece3aba8516000cf21bc1863
Mike Allen <mikefacesny@gmail.com>
2026-01-11 16:18:07 -0500

    edit babel config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withRorkMetro(config);
+module.exports = withRorkMetro(
+  withNativeWind(config, { input: "./global.css" })
+);
