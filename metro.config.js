a09e84d83b6b5c6e4efcf007f53adcbce0d9f082
Mike Allen <mikefacesny@gmail.com>
2026-01-11 18:47:38 -0500

    add nativewind

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withRorkMetro(config);
+module.exports = withRorkMetro(withNativeWind(config, { input: "./global.css" }));
