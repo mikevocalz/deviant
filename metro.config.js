d2dda5c8f7779e294b5eb9fe31f3dd1230e1957b
Mike Allen <mikefacesny@gmail.com>
2026-01-11 17:19:48 -0500

    update profile

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withRorkMetro(config);
+// Add .riv file support for Rive animations
+config.resolver.assetExts.push("riv");
+
+module.exports = withRorkMetro(withNativeWind(config, { input: "./global.css" }));
